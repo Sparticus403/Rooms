5db0deb0954c9f17dd9a25b9f78618764700338e
Sparticus403 <toddbwilliamson@gmail.com>
2022-12-29 11:41:37 -0700

    created currentRoom object to keep track of the current room

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include "room.h"
 
 int main(){
-    room myRoom = room("First Room");
+    room currentRoom = room("First Room");
     // std::cout << myRoom.get_room_name() << std::endl;
-    myRoom.add_north_room();
-    std::cout << myRoom.get_north_room() << std::endl;
-    std::cout << myRoom.get_east_room() << std::endl;
+    currentRoom.add_north_room();
+    // std::cout << myRoom.get_north_room() << std::endl;
+    // std::cout << myRoom.get_east_room() << std::endl;
+    currentRoom = *currentRoom.get_north_room();
+
+    std::cout << currentRoom.get_room_name() << std::endl;
+
     return 0;
 }
